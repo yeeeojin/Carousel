11e9874747a2cac5999604157ad1e3cd1df40bc1
yjhwnag <goom1901@hanmail.net>
2019-08-26 15:52:38 +0900

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# fifl-example
+# fifl-carousel
 
 ## Project setup
 ```
---
+++ src/components/Carousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Carousel</h3>
+    <div class="carousel">
+      <ul>
+        <li><img src="https://picsum.photos/id/1/400/300"></li>
+      </ul>
+      <button class="button-prev">{{ lt }}</button>
+      <span>사진 1</span>
+      <button class="button-next">{{ rt }}</button>
+    </div>
+
+    <h3>이미지 삭제</h3>
+    <div>
+      <label>
+        id: <input type="text">
+      </label>
+      <button>삭제</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  data () {
+    return {
+      lt: '<',
+      rt: '>'
+    }
+  }
+}
+</script>
