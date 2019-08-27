08063161f30baf323dd68b0ba354858fe9435f48
yjhwnag <goom1901@hanmail.net>
2019-08-27 14:23:16 +0900

    store 추가

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// modules
+import photoStore from './store/modules/photoStore'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    photoStore
+  },
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {
+  }
+})
